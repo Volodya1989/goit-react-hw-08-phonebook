7c9a4ba435d795d2235595541745a4c2c0793b8c
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-08-22 19:35:32 -0400

    hooks added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ import { Layout } from "../Layout";
 import { PrivateRoute } from "../PrivateRoute";
 import { RestrictedRoute } from "../RestrictedRoute";
 import { refreshUser } from "redux/auth/operations";
-import { useAuth } from "hooks";
+import { useAuth } from "../../hooks";
 
 const HomePage = lazy(() => import("../../pages/Home"));
 const RegisterPage = lazy(() => import("../../pages/Register"));
