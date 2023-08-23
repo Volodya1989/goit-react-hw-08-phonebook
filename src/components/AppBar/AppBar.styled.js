1d01a17b2202e91b602aa521360818dcfe4723ae
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-08-22 22:47:34 -0400

    navigation styled

--- src/components/AppBar/AppBar.styled.js
+++ src/components/AppBar/AppBar.styled.js
@@ -6,4 +6,9 @@ export const Header = styled.header`
   align-items: center;
   margin-bottom: 16px;
   border-bottom: 1px solid #2a363b;
+  background: linear-gradient(to bottom right, #fffcff, #d5fefd);
+  padding-left: 50px;
+  padding-right: 50px;
+  box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
+  opacity: 0.9;
 `;
